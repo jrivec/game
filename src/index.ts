d06f6a9883947e6289f15d33f6c61754d833bd4d
jrivec <61019020+jrivec@users.noreply.github.com>
2020-02-19 19:36:17 +0000

    game works

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
-
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+
+const secretNumber = getRandomInt(1, 9); //Math.floor(Math.random() * 10);
+const squares = document.querySelectorAll('.square') as NodeListOf<HTMLDivElement>;
+
+function getRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let currentSquare = 1;
+squares.forEach(sq => {
+    if (currentSquare === secretNumber) {
+        sq.dataset.secret = 'true';
+    }
+    currentSquare++;
+    sq.addEventListener('click', handleClick);
+});
+
+function handleClick() {
+    //console.log('got clicked', this);
+    const that = this as HTMLDivElement;
+    if (that.dataset.secret) {
+        that.classList.add('winner');
+        squares.forEach(x => x !== that ? x.classList.add('loser') : x);
+    }
+    else {
+        that.classList.add('loser');
+        that.removeEventListener('click', handleClick);
+    }
+}
 
